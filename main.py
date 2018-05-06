94ef1b72354a7b058fbd10a677ba265e0bbd5eab
Ealon1996 <38780025+Ealon1996@users.noreply.github.com>
2018-05-06 21:56:15 +0800

    discribe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import numpy as np
+import numpy as np  #宣告Numpy套件
 
-from PIL import Image
+from PIL import Image #宣告Python Image Library 套件
 
-
+#開檔動作
 
 ep = Image.open("Eprime.png")
 
@@ -38,39 +38,34 @@ print(e.format, e.size ,e.mode)
 
 print("\nstart decryption...")
 
-epoch=1
-
+epoch=1 #初始epoch
+#將圖片讀入np.array
 eparray=np.array(ep)
-print(eparray.shape,eparray)
 k1array=np.array(k1)
-print(k1array.shape,k1array)
 k2array=np.array(k2)
-print(k2array.shape,k2array)
 Iarray=np.array(img)
-print(Iarray.shape,Iarray)
 Earray=np.array(e)
-print(Earray.shape,Earray)
 
-
+#初始權重w
 w=np.array([1,1,1])
-print(w.shape,w)
 
-eptest = np.random.randn(300,400)
-
-for epoch in range(100):
+#利用gradient descent 解碼 每個pixel檢測一次 做十次
+for epoch in range(10):
     for k in range(300):
         for p in range(400):
             x=np.array([k1array[k,p],k2array[k,p],Iarray[k,p]])
-            xt=np.transpose(x)
-            a=np.dot(w,xt)
-            etemp=Earray[k,p]-a
-            w=w+0.0000001*etemp*x
+            xt=np.transpose(x)  #轉置以便相乘
+            a=np.dot(w,xt) #w*xt
+            etemp=Earray[k,p]-a #loss function
+            w=w+0.00001*etemp*x  #修改權重
 
-
+#轉置w
 w = np.transpose(w)
 
 
-
+#宣告eptest用來測試每個像素的值
+eptest = np.random.randn(300,400)
+#套用公式還原每個pixel的值
 for k in range(300):
 
     for p in range(400):
@@ -85,12 +80,11 @@ for k in range(300):
 
             eptest[k,p] = 0
 
-
-
-
+#印出權重
+print(w[0],"\n",w[1],"\n",w[2])
 
 eptest = np.array(eptest,dtype=np.uint8)
 
 etest = Image.fromarray(eptest)
 
-etest.save("Iprime.png")
+etest.save("Iprime1.png")
